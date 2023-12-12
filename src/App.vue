14bad476586377fa28e5eae451df6fe145c618ce
hanani8 <hananidasilva@gmail.com>
2023-12-12 17:21:47 +0530

    Established a layout.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
-  <div id="app">
-    <router-view/>
-    <nav>
-      <router-link to="/">Recommendations</router-link> |
-      <router-link to="/learning_paths">Learning Paths</router-link> |
-      <router-link to="/courses">Courses</router-link>
-    </nav>
+  <div id="app" class="mx-auto max-w-md min-h-screen flex flex-col border-2 border-black">
+    <!-- Header Section -->
+    <header class="flex items-center justify-between p-4 bg-gray-800 text-white">
+      <h1 class="text-2xl font-bold">DataForge</h1>
+      <div class="relative">
+        <button @click="toggleDropdown">
+            <img src="@/assets/person_1.svg" alt="Person Icon" class="w-8 h-8 text-white" />
+        </button>
+        <!-- Dropdown -->
+        <div v-if="showDropdown" class="absolute right-0 mt-2 bg-white shadow-md rounded-md p-2">
+          <!-- Dropdown content goes here -->
+          <a href="/logout">Logout</a>
+          <!-- You can customize the dropdown content as needed -->
+        </div>
+      </div>
+    </header>
+
+    <main class="flex-1 overflow-y-auto">
+      <router-view/>
+    </main>
+
+    <!-- USE DIVIDE-X-WIDTH -->
+    <!-- Footer Section -->
+    <footer class="flex justify-between p-4 bg-gray-800 text-white">
+      <!-- Three equally spaced buttons -->
+      <button class="flex-1 mx-1 p-2 bg-blue-500 hover:bg-blue-700 rounded-md"><router-link to="/" class="flex flex-row"><img src="@/assets/person.png" class="w-6 h-6"><span class="text-black font-bold text-md">Reccos</span></router-link></button>
+      <button class="flex-1 mx-1 p-2 bg-green-500 hover:bg-green-700 rounded-md"><router-link to="/learning_paths" class="flex flex-row"><img src="@/assets/person.png" class="w-6 h-6"><span class="text-black font-bold text-md">Paths</span></router-link></button>
+      <button class="flex-1 mx-1 p-2 bg-red-500 hover:bg-red-700 rounded-md"><router-link to="/courses" class="flex flex-row"><img src="@/assets/person.png" class="w-6 h-6"><span class="text-black font-bold text-md">Courses</span></router-link></button>
+    </footer>
+
   </div>
 </template>
+
+<style src="./style.css"></style>
 
 <style>
 #app {
@@ -16,6 +41,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: bisque;
+  height: 100%
 }
 
 nav {
@@ -31,3 +58,18 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      showDropdown: false,
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown;
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <div>
-      <input type="text" v-model="input1" placeholder="Input 1">
-      <input type="text" v-model="input2" placeholder="Input 2">
-      <input type="text" v-model="input3" placeholder="Input 3">
+  <div class="home flex flex-col">
+    <div class="font-bold text-xl float-left ml-2 mt-2">Preferences</div>
+    <div class="flex flex-col gap-3">
+      <input class="outline outline-2 h-16 mx-6" type="text" v-model="input1" placeholder="Input 1">
+      <input class="outline outline-2 h-16 mx-6" type="text" v-model="input2" placeholder="Input 2">
+      <input class="outline outline-2 h-16 mx-6" type="text" v-model="input3" placeholder="Input 3">
     </div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="redirectToRecommendations">Submit</button>
+    <!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="redirectToRecommendations">Submit</button> -->
   </div>
 </template>
 
