7d7e7ab7e71ad152cd8a4acb5fb6ad2c3fefdd95
hanani8 <hananidasilva@gmail.com>
2023-12-12 15:42:51 +0530

    Setup VueX

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import auth from "@/store/modules/auth.js"
+import learning_paths from "@/store/modules/learning_paths.js"
+import courses from "@/store/modules/courses.js"
+import students from "@/store/modules/students.js"
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    auth,
+    learning_paths,
+    courses,
+    students
+  }
+})
