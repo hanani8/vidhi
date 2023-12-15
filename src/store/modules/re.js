1192abde6bd93badf310da11ea35cd2d3cd3e9d5
hanani8 <hananidasilva@gmail.com>
2023-12-15 20:18:15 +0530

    Future Courses as Select

--- src/store/modules/re.js
+++ src/store/modules/re.js
@@ -2,15 +2,20 @@ import axios from 'axios';
 
 const state = {
     Recommendations: [],
+    future_courses: []
 };
 
 const getters = {
     getRecommendations: (state) => state.Recommendations,
+    getFutureCourses: (state) => state.future_courses
 }
 
 const mutations = {
     setRecommendations: (state, Recommendations) => {
         state.Recommendations = Recommendations;
+    },
+    setFutureCourses: (state, future_courses) => {
+        state.future_courses = future_courses;
     }
 }
 
@@ -22,11 +27,21 @@ const actions = {
             const pref_co = data.pref_co;
             const response = await axios.get(`recommendations?roll_no=${roll_no}&pref_no=${pref_no}&pref_co=${pref_co}`);
             const Recommendations = response.data;
-            console.log(pref_no, pref_co, roll_no);
 
             commit('setRecommendations', Recommendations);
         } catch (error) {
             console.log("Error getting recommendation:", error);
+        }
+    },
+    fetchFutureCourses: async({ commit }, data) => {
+        try {
+            const roll_no = data.roll_no;
+            const response = await axios.get(`get_future_courses/${roll_no}`);
+            const future_courses = response.data;
+
+            commit('setFutureCourses', future_courses);
+        } catch (error) {
+            console.log("Error getting future courses:", error);
         }
     }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,16 @@
   <div class="home flex flex-col">
     <div class="font-bold text-xl float-left ml-2 mt-2">Preferences</div>
     <div class="flex flex-col gap-3">
-      <input class="outline outline-2 h-16 mx-6" type="text" v-model="rollNumber" placeholder="Roll number">
+      <input v-if="isAuthenticated != true" class="outline outline-2 h-16 mx-6" type="text" v-model="rollNumber" placeholder="Roll number">
       <input class="outline outline-2 h-16 mx-6" type="text" v-model="preferredNumber" placeholder="Preferred number of courses">
-      <input class="outline outline-2 h-16 mx-6" type="text" v-model="preferredCourse" placeholder="Preferred course">
+      
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="getFutureCourses">get future courses</button>
+
+    <select class="outline outline-2 h-16 mx-6" v-model="preferredCourse">
+        <option value="" disabled selected>Select a course</option>
+        <option v-for="course in futureCourses" :value="course" :key="course">{{ course }}</option>
+      </select>
+    
     </div>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="getRecommendations">Submit</button>
   </div>
@@ -12,7 +19,7 @@
 
 <script>
 
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'HomeView',
@@ -23,18 +30,30 @@ export default {
       preferredCourse: ''
     }
   },
+  computed: {
+    ...mapGetters(["isAuthenticated", "user"]),
+    futureCourses: function() {
+      return this.$store.getters.getFutureCourses;
+    }
+  },
   methods: {
-    ...mapActions(["fetchRecommendations"]),
+    ...mapActions(["fetchRecommendations", "fetchFutureCourses"]),
 
     getRecommendations() {
       const info = {
-          roll_no: this.rollNumber,
+          roll_no: this.isAuthenticated ? this.user.user.name : this.rollNumber,
           pref_no: this.preferredNumber,
           pref_co: this.preferredCourse
           };
       this.fetchRecommendations(info);
       this.$router.push('/recommendations')
+    },
+    getFutureCourses() {
+      const info = {
+        "roll_no": this.isAuthenticated ? this.user.user.name : this.rollNumber,
+      }
+      this.fetchFutureCourses(info);
     }
-  }
+  },
 }
 </script>
